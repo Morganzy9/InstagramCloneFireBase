93f893233bba3e1d25719f2c29a1eb241fff9651
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-05-29 21:32:52 +0500

    Initial Commit

---
+++ InstagramCloneFireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramCloneFireBase
+//
+//  Created by Ислам Пулатов on 5/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
