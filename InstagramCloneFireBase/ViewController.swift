edc691c9e50d4309ee9a6be6f68d9b16729e080c
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-05-31 13:05:11 +0500

    -Make Sign In/Out functions

--- InstagramCloneFireBase/ViewController.swift
+++ InstagramCloneFireBase/ViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     @IBOutlet var emailTextField: UITextField!
     
-    @IBOutlet var PasswordTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
     
     
     override func viewDidLoad() {
@@ -22,13 +23,73 @@ class ViewController: UIViewController {
     }
 
     @IBAction func signInButtonClicked(_ sender: Any) {
+        
+        if emailTextField.text! != "" && passwordTextField.text! != "" {
+            
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { authDataResult, error in
+                
+                if error != nil {
+                    
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Unlnown Error occurs")
+                    
+                } else {
+                    
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                    
+                }
+                
+            }
+            
+            
+            
+        } else {
+            
+            makeAlert(title: "Empty Email or Password", message: "Please Enter the Email and Password")
+            
+        }
+ 
+        
         performSegue(withIdentifier: "toFeedVC", sender: nil)
         
         
     }
     @IBAction func signUpButtonClicked(_ sender: Any) {
         
+        if emailTextField.text! != "" && passwordTextField.text! != "" {
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { authDataResult, error in
+                
+                if error != nil {
+                    
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Unknown Error occurs")
+                    
+                }
+                
+                
+                self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+
+            }
+            
+        } else {
+            
+            makeAlert(title: "Empty Email or Password", message: "Please Enter the Email and Password")
+            
+        }
         
+        
+        
+    }
+    
+    
+    
+    func makeAlert(title : String, message : String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "Ok", style: .default)
+        
+        alert.addAction(ok)
+        
+        present(alert, animated: true)
         
     }
     
