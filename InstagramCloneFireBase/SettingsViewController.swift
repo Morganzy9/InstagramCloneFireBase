28bad3e3297ef1e69cb4879ee02861256a437a26
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-06-01 12:34:37 +0500

    -Added function to user t pick the image from Gallery

--- InstagramCloneFireBase/SettingsViewController.swift
+++ InstagramCloneFireBase/SettingsViewController.swift
@@ -115,6 +115,7 @@ class SettingsViewController: UIViewController {
             
         }
         
+        
     }
     
     
--- InstagramCloneFireBase/UploadViewController.swift
+++ InstagramCloneFireBase/UploadViewController.swift
@@ -6,19 +6,72 @@
 //
 
 import UIKit
+import Firebase
 
-class UploadViewController: UIViewController {
 
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var postImageButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        postImageButton.isEnabled = false
+        
+        
+        let gestureHideKeyboard = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureHideKeyboard)
+        
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+        
+    }
+
+//        MARK: Posting and Saving
+    
+    @IBAction func postImageButtonTapped(_ sender: Any) {
+        
+
+        
         
     }
     
-
+    
+    @objc func hideKeyboard() {
+        
+        view.endEditing(true)
+        
+    }
     
     
-
-
+    @objc func selectImage() {
+        
+        let vc = UIImagePickerController()
+        
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = false
+        
+        present(vc, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.originalImage] as? UIImage
+        postImageButton.isEnabled = true
+        self.dismiss(animated: true)
+        
+    }
+    
 
 }
